2622fb3bfe98e07fa61bc1de4fde6e81980b1199
David Moore <eroomd@gmail.com>
2020-09-02 15:56:25 -0400

    Workshop 300 - httpClient

--- src/app/dashboard/video-dashboard/video-dashboard.component.ts
+++ src/app/dashboard/video-dashboard/video-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { VideoData, videoData } from '../../video-data';
+import { VideoData } from '../../video-data';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-video-dashboard',
@@ -7,11 +8,17 @@ import { VideoData, videoData } from '../../video-data';
   styleUrls: ['./video-dashboard.component.scss'],
 })
 export class VideoDashboardComponent implements OnInit {
-  videoList = videoData;
+  videoList;
 
   currentVideo: VideoData;
 
-  constructor() {}
+  constructor(httpClient: HttpClient) {
+    httpClient
+      .get('https://api.angularbootcamp.com/videos')
+      .subscribe((videoList) => {
+        this.videoList = videoList;
+      });
+  }
 
   ngOnInit(): void {}
 
